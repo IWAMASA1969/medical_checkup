a947204c235da32c968c8711e3717321d9de5c9c
Hitoshi Iwamasa <hitoshi.iwamasa.vvat@willway.jp>
2018-09-27 18:01:18 +0900

    delete code for debuging

--- src/app/controllers/exam/items_controller.rb
+++ src/app/controllers/exam/items_controller.rb
@@ -2,8 +2,6 @@ class Exam::ItemsController < ApplicationController
   protect_from_forgery :except => [:create]
 
   def create
-p  JSON.parse(request.body.read)
-
     exam_item_service = ExamItemService.new
     added_exam_item = exam_item_service.add JSON.parse(request.body.read)
     render :json => added_exam_item
