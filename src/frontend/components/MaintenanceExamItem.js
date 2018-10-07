d44d7e6c05da0776ce428e15aebaf928a0753731
Hitoshi Iwamasa <masaxyz_labo@icloud.com>
2018-10-07 16:33:59 +0900

    create directory for maintenance exam item view

--- src/frontend/components/MaintenanceExamItem.js
+++ src/frontend/components/MaintenanceExamItem.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import MCAppBar from './commons/MCAppBar'
+
 class MaintenanceExamItem extends React.Component {
   render () {
     return (
       <div>
-        <AppBar position="static" color="default">
-          <Toolbar>
-            <Typography variant="title" color="inherit">
-              Exam Item
-            </Typography>
-          </Toolbar>
-        </AppBar>
+        <MCAppBar title={"Exam Item"} />
       </div>
     );
   }
---
+++ src/frontend/components/commons/MCAppBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+class MCAppBar extends React.Component {
+  render () {
+    return (
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              {this.props.title}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default MCAppBar
