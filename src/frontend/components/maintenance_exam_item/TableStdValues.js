ae34efe9553d4af415056d69d12d8ae5b35c1c30
Hitoshi Iwamasa <masaxyz_labo@icloud.com>
2018-10-20 12:13:14 +0900

    add style

--- src/frontend/components/maintenance_exam_item/TableStdValues.js
+++ src/frontend/components/maintenance_exam_item/TableStdValues.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
-//const styles = theme => ({
+const styles = theme => ({
 ////  root: {
 ////    width: '100%',
 ////    marginTop: theme.spacing.unit * 3,
 ////    overflowX: 'auto',
 ////  },
-//  table: {
-//    minWidth: 700,
-//  },
-//});
+  table: {
+    minWidth: 700,
+  },
+});
 
 const rows = [
       { 'id':1, 'from':0, 'to':149, 'value':'A' },
       { 'id':2, 'from':150, 'to':400, 'value':'B' },
     ];
+
 class TableStdValues extends React.Component {
 
   render () {
+    const { classes } = this.props;
+
     return (
       <div>
-        <Table>
+        <Table className={classes.table}>
           <TableHead>
             <TableRow>
               <TableCell numeric>from</TableCell>
@@ -48,20 +51,13 @@ class TableStdValues extends React.Component {
           </TableBody>
         </Table>
       </div>
-//        <TableBody>
-//          {rows.map(row => {
-//            return (
-//              <TableRow key={row.id}>
-//                <TableCell numeric>{row.calories}</TableCell>
-//                <TableCell numeric>{row.fat}</TableCell>
-//                <TableCell>{row.value}</TableCell>
-//              </TableRow>
-//            );
-//          })}
-//        </TableBody>
     );
   }
 }
 
-export default TableStdValues;
-//export default withStyles(styles)(TableStdValues);
+TableStdValues.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+//export default TableStdValues;
+export default withStyles(styles)(TableStdValues);
